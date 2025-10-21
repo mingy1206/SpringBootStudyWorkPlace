1469b94bf6ac48a2793eb2cb82c6f0e2fafef98a
이민기 <mingy837@gmail.com>
2025-10-21 17:29:58 +0900

    '7.2블로그 글 목록 뷰 구현하기' 구현

--- src/main/java/me/leemingi/springbootdeveloper/domain/Article.java
+++ src/main/java/me/leemingi/springbootdeveloper/domain/Article.java
@@ -1,15 +1,16 @@
 package me.leemingi.springbootdeveloper.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity // 엔티티로 지정
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,6 +23,11 @@ public class Article {
     private String title;
     @Column(name="content",nullable = false)
     private String content;
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Builder // 빌더 패턴으로 객체 생성
     public Article(String title, String content){
