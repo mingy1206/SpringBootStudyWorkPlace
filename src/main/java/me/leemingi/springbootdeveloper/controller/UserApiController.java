3721cd0e5187e951dcec30cdf2fd4396393c5113
이민기 <mingy837@gmail.com>
2025-10-22 17:59:20 +0900

    8.4까지 완성

---
+++ src/main/java/me/leemingi/springbootdeveloper/controller/UserApiController.java
@@ -0,0 +1,18 @@
+package me.leemingi.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.leemingi.springbootdeveloper.dto.AddUserRequest;
+import me.leemingi.springbootdeveloper.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+    private final UserService userService;
+    @PostMapping("/user")
+    public String signup(AddUserRequest request){
+        userService.save(request); //회원 가입 메서드 호출
+        return "redirct:/login"; // 회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+}
